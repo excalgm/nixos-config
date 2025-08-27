55cb79f799dc7b9845cedc8ef6fe6dd90b2e295e
Excal <excalgames@duck.com>
2025-08-27 17:23:34 +0300

    Add Prism Launcher to PC config

--- hosts/b450m/local-packages.nix
+++ hosts/b450m/local-packages.nix
@@ -2,5 +2,12 @@
   environment.systemPackages = with pkgs; [
     vim
     ntfs3g
+    (prismlauncher.override {
+      jdks = [
+        jdk21
+      ];
+
+      gamemodeSupport = true;
+    })
   ];
 }
