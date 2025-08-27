d9a35dc887cf9162513a94675bf77efeaeb1d670
Excal <excalgames@duck.com>
2025-08-27 17:26:44 +0300

    Fix Vim settings
    Previous settings commit doesn't work

--- home-manager/modules/apps/vim.nix
+++ home-manager/modules/apps/vim.nix
@@ -2,7 +2,6 @@
   programs.vim = {
     enable = true;
     settings.expandtab = true;
-    settings.smarttab = true;
     settings.shiftwidth = 2;
     settings.tabstop = 8;
     settings.softtabstop = 0;
