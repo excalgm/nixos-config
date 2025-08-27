e0f581180a664271f90d7f5780766b6b4bfc2d67
Excal <excalgames@duck.com>
2025-08-27 20:16:05 +0300

    Replace vim full with base vim

--- home-manager/modules/apps/vim.nix
+++ home-manager/modules/apps/vim.nix
@@ -1,6 +1,7 @@
-{
+{ pkgs, ... }: {
   programs.vim = {
     enable = true;
+    packageConfigurable = pkgs.vim;
     settings.expandtab = true;
     settings.shiftwidth = 2;
     settings.tabstop = 8;
