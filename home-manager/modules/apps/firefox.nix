9053dcf5d33d24f88641d3547b4ed892329a0bda
Excal <excalgames@duck.com>
2025-09-11 15:49:07 +0300

    Replace GNOME Browser Integration with Plasma

--- home-manager/modules/apps/firefox.nix
+++ home-manager/modules/apps/firefox.nix
@@ -2,7 +2,7 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox.override {
-      nativeMessagingHosts = [ pkgs.gnome-browser-connector ];
+      nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
     };
     profiles.default = {
       isDefault = true;
