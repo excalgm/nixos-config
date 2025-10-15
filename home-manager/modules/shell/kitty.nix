2a369ae0a9d0e9f0f7792443e5ff08dae0b7b0e8
Excal <excalgames@duck.com>
2025-10-15 17:09:30 +0300

    Remove tabs in kitty

--- home-manager/modules/shell/kitty.nix
+++ home-manager/modules/shell/kitty.nix
@@ -4,11 +4,6 @@
     enableGitIntegration = true;
     settings = {
       hide_window_decorations = "no";
-      tab_bar_min_tabs = 1;
-      tab_bar_edge = "bottom";
-      tab_bar_style = "powerline";
-      tab_powerline_style = "slanted";
-      tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
     };
   };
 }
