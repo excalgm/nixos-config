626019c99f6efe12e79f8021f6e5e4ee08bcd547
Excal <excalgames@duck.com>
2025-11-22 13:15:48 +0300

    Add hyprlock theme

--- home-manager/modules/hyprland/hyprlock.nix
+++ home-manager/modules/hyprland/hyprlock.nix
@@ -1,32 +1,49 @@
-{
+{ config, ... }: {
   programs.hyprlock = {
     enable = true;
     settings = {
+      source = [
+        "${config.programs.matugen.theme.files}/.config/hypr/colors.conf"
+      ];
+
+      "$font" = "Rubik Medium";
+
       general = {
         hide_cursor = true;
         grace = 5;
       };
+
       background = [
         {
-        color = "rgb(282828)";
+        color = "$surface";
         }
       ];
+
       label = [
         {
           text = "$LAYOUT";
-          color = "rgb(ebdbb2)";
+          color = "$on_surface";
           font_size = 14;
-          font_family = "JetBrainsMono Nerd Font";
+          font_family = "$font";
           position = "-30, 30";
           halign = "right";
           valign = "bottom";
         }
         {
           text = "$TIME";
-          color = "rgb(ebdbb2)";
+          color = "$on_surface";
           font_size = 90;
-          font_family = "JetBrainsMono Nerd Font";
+          font_family = "$font";
           position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          text = "cmd[update:43200000] date +\"%A, %d %B %Y\"";
+          color = "$on_surface";
+          font_size = 25;
+          font_family = "$font";
+          position = "-30, -150";
           halign = "right";
           valign = "top";
         }
@@ -38,15 +55,16 @@
           dots_size = 0.2;
           dots_spacing = 0.2;
           dots_center = true;
-          outer_color = "rgb(b8bb26)";
-          inner_color = "rgb(665c54)";
-          font_color = "rgb(ebdbb2)";
+          outer_color = "$outline_variant";
+          inner_color = "$surface_container";
+          font_color = "$on_surface_variant";
           fade_on_empty = false;
+          placeholder_text = "Password for <i>$USER</i>";
           hide_input = false;
-          check_color = "rgb(b8bb26)";
-          fail_color = "rgb(fb4934)";
+          check_color = "$secondary";
+          fail_color = "$error";
           fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-          capslock_color = "rgb(fabd2f)";
+          capslock_color = "$tertiary";
           position = "0, -47";
           halign = "center";
           valign = "center";
